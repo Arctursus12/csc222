78ec76f0e77535a8edb06e4d9f124c67c58acc7e
Arc <40873828+Arctursus12@users.noreply.github.com>
2024-11-14 09:57:42 -0500

    Implemented basic function to clear tests 1/2/3

--- Ch10_Ex2/num_vector_utils.cpp
+++ Ch10_Ex2/num_vector_utils.cpp
@@ -2,7 +2,17 @@
 #include <vector>
 using namespace std;
 
-string render_num_vector(const vector<int>&) {
-    string s = "";
+string render_num_vector(const vector<int>& vec) {
+    if (vec.empty()) {
+        return "";
+    } /*Returning an empty string if the vector is empty, accounting for test 3
+    Not 100% sure this is how the "empty" string would look, maybe a null value would be better*/
+    string s;
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (i > 0) {
+            s += ' ';
+        }
+        s += to_string(vec[i]); //Converting the integer to a string and appending, tests 1/2
+    }
     return s;
 }
